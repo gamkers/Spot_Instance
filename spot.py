46cf259e3281368ff2f9b6a18c1d9e96ffabc0e3
AKASH <87299311+gamkers@users.noreply.github.com>
2023-09-09 21:52:38 +0530

    Update spot.py

--- spot.py
+++ spot.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import subprocess
-
 import os
 
 # Set the file permissions to allow execution
@@ -12,35 +11,36 @@ try:
 except Exception as e:
     print(f"Error: {e}")
 
-
+# Function to run spotinfo
 def run_spotinfo(args):
-    # Call the shell script and capture the output
     result = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return result.stdout.decode('utf-8'), result.stderr.decode('utf-8')
 
-st.title("AWS Spot Instance Calculator")
+# Set page title and add some space on the left and right
+st.title(" AWS Spot Instance Calculator ")
+st.write(" ")
 
-# Get user input
-st.write("Choose EC2 instance type:")
-instance_choice = st.selectbox("Choose EC2 instance type:", [
+# Create a sidebar for user input
+st.sidebar.title("Settings")
+instance_choice = st.sidebar.selectbox("Choose EC2 instance type:", [
     "t2.micro", "t2.small", "t2.medium", "t3.nano",  # Add all choices
     "t3.micro", "t3.small", "t3.medium", "c5.large",
     # ... add more choices ...
 ], index=0)
 
-os_choice = st.selectbox("Choose instance operating system:", ["Windows", "Linux", "macOS"])
-region = st.text_input("Enter AWS region (use 'all' for all regions)", "us-east-1")
-output = st.selectbox("Enter output format", ["number", "text", "json", "table", "csv"], index=3)
-cpu = st.slider("Enter minimal vCPU cores", min_value=0, max_value=64, value=0)
-memory = st.slider("Enter minimal memory GiB", min_value=0, max_value=256, value=0)
-price = st.slider("Enter maximum price per hour", min_value=0.0, max_value=10.0, value=0.0)
-sort = st.selectbox("Enter sort order", ["interruption", "type", "savings", "price", "region"], index=0)
-order = st.selectbox("Enter sort order", ["asc", "desc"], index=0)
+os_choice = st.sidebar.selectbox("Choose instance operating system:", ["Windows", "Linux", "macOS"])
+region = st.sidebar.text_input("Enter AWS region (use 'all' for all regions)", "us-east-1")
+output = st.sidebar.selectbox("Enter output format", ["number", "text", "json", "table", "csv"], index=3)
+cpu = st.sidebar.slider("Enter minimal vCPU cores", min_value=0, max_value=64, value=0)
+memory = st.sidebar.slider("Enter minimal memory GiB", min_value=0, max_value=256, value=0)
+price = st.sidebar.slider("Enter maximum price per hour", min_value=0.0, max_value=10.0, value=0.0)
+sort = st.sidebar.selectbox("Enter sort order", ["interruption", "type", "savings", "price", "region"], index=0)
+order = st.sidebar.selectbox("Enter sort order", ["asc", "desc"], index=0)
 
-if st.button("Run spotinfo"):
+if st.sidebar.button("Run spotinfo"):
     # Prepare the arguments for the shell script
     args = [
-        "./spotinfo","--type", instance_choice, "--os", os_choice, "--region", region,
+        "./spotinfo", "--type", instance_choice, "--os", os_choice, "--region", region,
         "--output", output, "--cpu", str(cpu), "--memory", str(memory),
         "--price", str(price), "--sort", sort, "--order", order
     ]
@@ -54,4 +54,3 @@ if st.button("Run spotinfo"):
     if error:
         st.text("Error:")
         st.code(error)
-
